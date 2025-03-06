51367d56b724bc71b81e2df061d4701f34053dad
E.Gopi <egopimani1998@gmail.com>
2025-03-06 16:40:30 +0530

    auto commit

--- git.sh
+++ git.sh
@@ -3,17 +3,16 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 ls_cmd=$(find . -maxdepth 1 -type d ! -name ".")
-# ls_cmd=$(ls -d *)
+cmd="git add . && git commit -m 'auto commit' && git push"
 git_push_repo() {
     for i in $ls_cmd ; do
         cd "$i" || continue
-
         if [ -d .git ]; then
             echo -e "on $G $i $N .git repo exist...$G pusing the code to remote repo $N"
-            git add . ; git commit -m "auto commit" ; git push
+            eval $cmd 1> /dev/null
             cd ..
             continue
-        else n
+        else
             echo -e "on $R $i $N .git repo not exist...$Y Skipping the $i $N"
             cd ..
             continue
